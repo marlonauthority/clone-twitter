3d828cf858d7a989922f8aee81cd8e315ee33df6
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-10-17 18:20:21 -0400

    att

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //inicializando o app
 const app = express();
 
 //realtime
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 //conectando a database
-mongoose.connect(
-    "mongodb://<user>:<password>@mlab.com",
-    {
-        useNewUrlParser: true
-    }
-);
+mongoose.connect("mongodb://localhost:27017/mongotwitter", 
+  // mongodb://localhost:27017/mongotwitter
+  //mongodb://<user>:<password>@mlab.com
+{
+  useNewUrlParser: true
+});
 
 //variavel/middleware visivel a tudo, ela é passada pelo req
 app.use((req, res, next) => {
-    req.io = io;
-    //next continua o fluxo...
-    return next();
+  req.io = io;
+  //next continua o fluxo...
+  return next();
 });
 
 //habilitando cors
@@ -30,10 +30,9 @@ app.use(cors());
 // usa o json para requisicoes
 app.use(express.json());
 //rotas
-app.use(require('./routes'));
-
+app.use(require("./routes"));
 
 //servindo na porta
-server.listen('3000', () => {
-    console.log('Server started on port 3000 :D');
+server.listen("3000", () => {
+  console.log("Server started on port 3000 :D");
 });
